79c8e3f280ed435e6e645aca42ddcc6aa4f5b209
tam2110097 <nguyenminhtam301103@gmail.com>
2025-01-20 16:18:07 +0700

    video 29

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MyComponent from './components/learn/MyComponent'
+import { SecondComponent, ThirdComponent } from './components/learn/SecondComponent'
 
-//component = html + css + js
-const MyComponent = () => {
-  return (
-    <div>eric & hoidanit update</div>
-  );
-}
+
 
 const App = () => {
   const [count, setCount] = useState(0)
@@ -24,7 +21,9 @@ const App = () => {
         </a>
       </div>
       <h1>Hello world & Nguyễn Minh Tâm</h1>
-      <MyComponent/>
+      <MyComponent />
+      <SecondComponent />
+      <ThirdComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
