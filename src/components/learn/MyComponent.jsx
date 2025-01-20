51f972f39d2f15b6ed56f9e5df8a637a6500f7d0
tam2110097 <nguyenminhtam301103@gmail.com>
2025-01-20 16:26:59 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,19 @@
+
+//JSX
+//fragment
+
 //component = html + css + js
+import './style.css'
 const MyComponent = () => {
-  return (
-    <div>eric & hoidanit update</div>
-  );
+    return (
+        <>
+            <div>eric & hoidanit update</div>
+            <div className="child"
+                style={{ borderRadius: '10px' }}
+            >child</div>
+        </>
+
+    );
 }
 
 export default MyComponent;
