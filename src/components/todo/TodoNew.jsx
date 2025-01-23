f9bf556af193399e41f21b9d208d1b75990eb6ed
tam2110097 <nguyenminhtam301103@gmail.com>
2025-01-23 14:30:16 +0700

    video 35

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,5 +1,8 @@
 
-const TodoNew = () => {
+const TodoNew = (props) => {
+    console.log('>>>>>>>check props todo new: ', props)
+    const { addNewTodo } = props;
+    addNewTodo('tam')
     return (
         <div className="todo-new">
             <input type="text" />
