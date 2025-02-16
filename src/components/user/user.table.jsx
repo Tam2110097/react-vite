f2b5ce482c87554aa43d0965e96fed9e26abe685
tam2110097 <nguyenminhtam301103@gmail.com>
2025-02-16 14:41:57 +0700

    video 75

--- src/components/user/user.table.jsx
+++ src/components/user/user.table.jsx
@@ -1,16 +1,6 @@
 import { Space, Table, Tag } from 'antd';
-import { fetchAllUserAPI } from '../../services/api.service';
-import { useEffect, useState } from 'react';
-const UserTable = () => {
-
-    const [dataUsers, setDataUsers] = useState([])
-
-    //empty array = run once
-    useEffect(() => {
-        console.log('>>>>run useEffect')
-        loadUser();
-    }, []);
-
+const UserTable = (props) => {
+    const { dataUsers } = props;
     const columns = [
         {
             title: 'Id',
@@ -25,13 +15,6 @@ const UserTable = () => {
             dataIndex: 'email',
         },
     ];
-
-    const loadUser = async () => {
-        const res = await fetchAllUserAPI()
-        setDataUsers(res.data)
-    }
-
-    console.log('>>>>run render')
     return (
         <Table columns={columns}
             dataSource={dataUsers}
--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,11 +1,29 @@
 import UserForm from "../components/user/user.form";
 import UserTable from "../components/user/user.table";
+import { fetchAllUserAPI } from '../services/api.service';
+import { useEffect, useState } from 'react';
 
-const UserPage = () => {
+const UserPage = (props) => {
+    const [dataUsers, setDataUsers] = useState([])
+
+    //empty array = run once
+    useEffect(() => {
+        loadUser();
+    }, []);
+
+    const loadUser = async () => {
+        const res = await fetchAllUserAPI()
+        setDataUsers(res.data)
+    }
+
     return (
         <div style={{ padding: "20px" }}>
-            <UserForm />
-            <UserTable />
+            <UserForm
+                loadUser={loadUser}
+            />
+            <UserTable
+                dataUsers={dataUsers}
+            />
         </div>
     )
 }
